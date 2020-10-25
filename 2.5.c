e23122810f9b41ea5088a177425c4c00ca0e826a
SyafiqaZainal <fyqaza99@gmail.com>
2020-10-25 06:33:20 +0000

    commit lab 2.5

---
+++ 2.5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int main(void)
+{
+char name[20];
+
+for(int i=1; i<5; i++)
+{
+pid_t pid=fork();
+
+if(pid == 0)
+{
+printf("Enter your name> PPID=%d.PID=%d\n",getppid(), getpid());
+scanf("%s",name);
+printf("Your name is %s ",name);
+exit(0);
+}
+else
+{
+printf("Job process > PID=%d\n", getpid());
+printf("waiting for name processes to finish..\n");
+wait(NULL);
+printf("Job is done.\n");
+}
+}
+
+return EXIT_SUCCESS;
+}
+
